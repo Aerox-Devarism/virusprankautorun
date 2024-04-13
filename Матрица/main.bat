31f0aaa886a3b355460756fe0b0226537d904261
Aerox-Devarism <148233909+Aerox-Devarism@users.noreply.github.com>
2024-04-13 18:53:47 +0500

    Add files via upload

---
+++ Матрица/main.bat
@@ -0,0 +1,9 @@
+@echo off
+color 04
+echo Hello, I'm a hacker))))
+timeout 3 /nobreak >nul
+echo Let's start hacking your ass))))
+timeout 3 /nobreak >nul
+:matrix
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto matrix
